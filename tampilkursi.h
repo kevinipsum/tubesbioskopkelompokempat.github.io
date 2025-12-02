81794be7e5a78e93a2e4a53835e2e19534f63053
kevinipsum <azexxkevin@gmail.com>
2025-12-02 23:16:40 +0700

    Update tampilkursi.h

--- tampilkursi.h
+++ tampilkursi.h
@@ -3,12 +3,20 @@
 void tampilkankursi(int indexfilm) {
     printf("\n== Kursi untuk film: %s (Studio %d, Jam %s) ==\n",
            namafilm[indexfilm], studiofilm[indexfilm], jamfilm[indexfilm]);
+
     for (int i = 0; i < totalbaris; i++) {
         printf("%c: ", 'A' + i);
+
         for (int j = 0; j < totalkolom; j++) {
-            char buf[8];
-            snprintf(buf, sizeof(buf), "%s[%c]", labelkursi[i][j], status[indexfilm][i][j] ? 'T' : 'K');
-            printf("%-7s", buf);
+            char statusChar = status[indexfilm][i][j] ? 'T' : 'K';
+
+            const char *warna =
+                (statusChar == 'K') ? "\033[1;32m" : "\033[1;31m";  
+                
+            printf("%s%s[%c]\033[0m  ",
+                   labelkursi[i][j],
+                   warna,
+                   statusChar);
         }
         printf("\n");
     }
