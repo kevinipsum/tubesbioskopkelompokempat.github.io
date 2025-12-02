6ed5b31250913e95e4dcd9bf0bf386689184858f
Joicech <joicechristie08@gmail.com>
2025-12-02 22:13:55 +0700

    Add files via upload

---
+++ pilihfilm.h
@@ -0,0 +1,31 @@
+#include "define.h"
+
+const char *namafilm[jumlahfilm] = {
+    "Avengers Endgame",
+    "Wicked",
+    "Pengabdi Setan"
+};
+const int studiofilm[jumlahfilm] = {1, 2, 3};
+const char *jamfilm[jumlahfilm] = {"13.20", "14.50", "16.00"};
+
+char labelkursi[totalbaris][totalkolom][panjangkode];
+int status[jumlahfilm][totalbaris][totalkolom];
+
+int pilih() {
+    int pilihan;
+    printf("\n=== PILIH FILM ===\n");
+    for (int i = 0; i < jumlahfilm; i++) {
+        printf("%d. %s (Studio %d) - %s\n", i + 1, namafilm[i], studiofilm[i], jamfilm[i]);
+    }
+    printf("Pilih film (1-%d): ", jumlahfilm);
+    if (scanf("%d", &pilihan) != 1) {
+        printf("Input tidak valid!\n");
+        while (getchar() != '\n');
+        return -1;
+    }
+    if (pilihan < 1 || pilihan > jumlahfilm) {
+        printf("Pilihan film tidak valid\n");
+        return -1;
+    }
+    return pilihan - 1;
+}
